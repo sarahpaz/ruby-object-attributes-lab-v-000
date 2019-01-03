d335f50dc7416b4e20873043410c7a423ddb597e
Sarah Paz <sarah.paz@gmail.com>
2019-01-03 05:06:57 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,17 @@
 class Dog
-
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
 
   def name
     @name
   end
 
-  def breed=(dog_breed)
-    @breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end
 
   def breed
     @breed
   end
-
 end
