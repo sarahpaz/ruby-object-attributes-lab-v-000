80ddd80c9c030a698cc864643b7b80456476acc8
Sarah Paz <sarah.paz@gmail.com>
2019-01-03 05:02:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,26 @@
+class Dog
 
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end  
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+end
 
 
 
