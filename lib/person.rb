d3697c15526fc389e99bd4eb87649f0ff954fd74
Sarah Paz <sarah.paz@gmail.com>
2019-01-03 04:59:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,38 @@
-class Person
 
-  def name=(person_name)
-    @name = person_name
-  end
 
-  def name
-    @name
-  end
 
-  def job=(person_job)
-    @job = person_job
-  end
 
-  def job
-    @job
-  end
-  
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person
+#
+#   def name=(person_name)
+#     @name = person_name
+#   end
+#
+#   def name
+#     @name
+#   end
+#
+#   def job=(person_job)
+#     @job = person_job
+#   end
+#
+#   def job
+#     @job
+#   end
+#
+# end
